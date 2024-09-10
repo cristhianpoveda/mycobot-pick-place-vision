b448ce0056819f8eb41282ef57d71a92de31c0e8
cristhianpoveda <cristhianpoveda12@gmail.com>
2024-09-10 12:38:39 -0500

    previous debug 4

--- pick-place-pkgs/perception/src/perception/detection_node.py
+++ pick-place-pkgs/perception/src/perception/detection_node.py
@@ -144,9 +144,9 @@ class ObjectDetector():
 
                     picking_distance = math.sqrt(pose.position.x**2 + pose.position.y**2)
 
-                    rospy.loginfo(f"px: {abs(previous.previous.position.x)}, x: {abs(pose.position.x)}, py: {abs(previous.previous.position.y)}, y: {abs(pose.position.y)}")
+                    prev_dist = math.sqrt((previous.previous.position.x - pose.position.x)**2 + (previous.previous.position.y - pose.position.y)**2)
 
-                    if (abs(previous.previous.position.x) - abs(pose.position.x)) > 0.015 or (abs(previous.previous.position.y) - abs(pose.position.y)) > 0.015:
+                    if prev_dist > 0.02:
 
                         if picking_distance < 0.241:
 
